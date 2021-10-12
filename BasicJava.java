38f18a8ce6e616f967950955f829ceb3427be017
Shella <anandashella59@gmail.com>
2021-10-12 13:02:00 +0700

    project 1

---
+++ BasicJava.java
@@ -0,0 +1,21 @@
+public class BasicJava{
+    public static void main(String[] args) {
+        String firstName = "Shella ";
+        String lastName = "Ananda" ;
+        String address = " Jalan Sekata Glugur" ;
+        int age = 20 ;
+        String phoneNumber = " 083164208560" ;
+        String hobby = " drakoran, webtoon" ;
+        String favoriteFood = " soto, ayam lada hitam" ;
+        Double floatNumber =  100.99 ;
+        Boolean collegeStudent =  true ;
+        System.out.println("firstName     : " + firstName + lastName);
+        System.out.println("adress        : " + address);
+        System.out.println("age           : " + age);
+        System.out.println("phoneNumber   : " + phoneNumber);
+        System.out.println("hobby         : " + hobby);
+        System.out.println("favoriteFood  : " + favoriteFood);
+        System.out.println("floatNumber   : " + floatNumber);
+        System.out.println("collegeStudent: " + collegeStudent);
+    }
+}    
